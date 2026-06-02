d3f62b29b05bc47ed4a14d53166dd1a185c62d01
Ibrahim-Javed-ai850 <i1javed@torontomu.ca>
2026-06-02 08:15:45 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("AeroMind")
+st.subheader("AI-Powered UAV Mission Planning & Risk Assessment Platform")
+
+st.write("This app will help evaluate UAV mission risk based on battery level, wind speed, payload weight, distance, and environmental conditions.")
